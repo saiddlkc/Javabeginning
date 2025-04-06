dd0bab5e9a7501c35fd0170c81b249dcdd1cd97d
Said <ali.dalkilic@dci-student.org>
2025-04-06 14:16:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void  main (String[] args) {
+
+        //   System.out.println(2+2);
+        // System.out.print("Ich kann rechnen");
+    int age = 21 ; // Ganze zahlen
+        System.out.println(age);
+
+    double price = 18.4;  // komma zahlen und unvollständige
+    System.out.println(price);
+
+    boolean isStudent = false;
+    if(isStudent){
+        System.out.println("You are a student");
+    }else{
+            System.out.println("You are not a Student");
+        }
+
+    char grade = 'A';
+    char symbol= '$';
+        System.out.println("19.99"+symbol);
+
+    String name = "Said Ali Dalkilic";
+    String email = "sadaAdmin01@gmail.com";
+        System.out.println("Admins online : " + name  +" "+  email);
+    }
+
+
+}
