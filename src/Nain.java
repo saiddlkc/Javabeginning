c6df8bf9c5933500530c65a6998c47d3c9354928
saiddlkc <saidalkilic@gmail.com>
2025-04-07 11:15:09 +0200

    xswad

--- src/Nain.java
+++ src/Nain.java
@@ -4,30 +4,40 @@ public class Nain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        double widht = 5.0;
+        double height = 10.0;
+        double area = widht * height;
 
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Width of the rectangle: ");
+        widht = scanner.nextDouble();
+        System.out.print("Height of the rectangle: ");
+        height = scanner.nextDouble();
+        area = widht * height;
+        System.out.println("Area of the rectangle is: " + area);
 
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
+        // System.out.print("Enter your name: ");
+        // String name = scanner.nextLine();
 
-        System.out.print("Enter your height in cm: ");
-        double height = scanner.nextDouble();
+        // System.out.print("Enter your age: ");
+        // int age = scanner.nextInt();
 
-        System.out.print("Enter your weight in kg: ");
-        double weight = scanner.nextDouble();
+        // System.out.print("Are you a student? (true/false): ");
+        // boolean isStudent = scanner.nextBoolean();
 
-        System.out.println("Your name is: " + name);
-        System.out.println("Your age is: " + age);
-        if (isStudent) {
-            System.out.println("You are a student at BHT.");
-        } else {
-            System.out.println("You are  NOT a student ");
-        }
-        System.out.println("Your height is: " + height + " cm");
-        System.out.println("Your weight is: " + weight + " kg");
+        // System.out.println("Your name is: " + name);
+
+        // if (age < 18) {
+        // System.out.println("You are too young.");
+        // } else if (age >= 18 && age < 65) {
+        // System.out.println("You are an adult.");
+        // } else {
+        // System.out.println("You are a .");
+        // }
+        // if (isStudent) {
+        // System.out.println("You are a student at BHT.");
+        // } else {
+        // System.out.println("You are NOT a student ");
+        // }
 
         scanner.close();
 
